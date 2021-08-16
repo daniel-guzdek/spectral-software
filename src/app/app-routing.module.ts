f3e8477391959dba11f3a4fd696e12758a06a0c1
daniel-guzdek <daniel.guzdek.86@gmail.com>
2021-08-16 13:08:23 +0200

    Added Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesCompComponent } from './services-comp/services-comp.component';
+import { CareerComponent } from './career/career.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeroComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesCompComponent },
+  { path: 'career', component: CareerComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+export const routingComponents = [
+  HeroComponent,
+  AboutComponent,
+  ServicesCompComponent,
+  CareerComponent,
+  ContactComponent,
+];
